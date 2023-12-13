2ac1b77bbc599f4c7864d2181a34a114f5eb9167
heeneeee <gmldnjd0614@gmail.com>
2023-12-13 17:49:01 +0900

    add : 파일 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClientProvider, QueryClient } from "react-query";
+import HomePage from "./pages/HomePage";
 
-function App() {
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <HomePage />;
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
---
+++ src/api/todos.ts
@@ -0,0 +1,2 @@
+// 여기서 CRUD가 이루어질듯
+import axios from "axios";
